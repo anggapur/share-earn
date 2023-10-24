51a694da597182d73328dbd74b6d2b31b0396e14
anggapurz <angga+1@itsavirus.com>
2023-10-24 21:11:15 +0800

    allow cors

--- server/index.js
+++ server/index.js
@@ -37,6 +37,12 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 
+app.use(function(req, res, next) {
+	res.header("Access-Control-Allow-Origin", "*");
+	res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+	next();
+});
+
 app.use(
     cors({
         origin: config.CLIENT_URL,
